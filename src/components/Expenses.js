8f9631320333c99d9a034f21401ccf922f9f632f
HANA CHOI <hana40820@gmail.com>
2023-04-02 14:09:07 +0900

    Seperating Card UI

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Expenses.module.css";
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from './UI/Card';
 
 function Expenses(props) {
   return (
